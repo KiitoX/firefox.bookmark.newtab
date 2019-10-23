c143d429039382c2913193dfa289e76d5a8df2d0
MCManuelLP <mcmanuellp@gmail.com>
2019-10-23 17:24:37 +0200

    Closed tab cleanup now uses url instead of title

--- clearClosedTabs.js
+++ clearClosedTabs.js
@@ -1,13 +1,15 @@
 // Licensed as MIT
 // Source: https://github.com/mgwio/newtab.urlEX
 
+let newtabUrl = browser.extension.getURL("newtab.html");
+
 // this function hides closed new tabs from the "recently closed tabs" menu
 async function tabComms(p) {
 	async function expungeClosedTab(tab) {
 		let tabId = tab.tabId;
 		await browser.tabs.remove(tabId);
 		let closed = await browser.sessions.getRecentlyClosed();
-		if (Object.prototype.hasOwnProperty.call(closed[0], 'tab') && closed[0].tab.title === '@NewTab') {
+		if (Object.prototype.hasOwnProperty.call(closed[0], 'tab') && closed[0].tab.url === newtabUrl) {
 			browser.sessions.forgetClosedTab(
 				closed[0].tab.windowId,
 				closed[0].tab.sessionId.toString()
@@ -18,4 +20,4 @@ async function tabComms(p) {
 	p.addEventListener("message", expungeClosedTab);
 }
 
-browser.runtime.addEventListener("connect", tabComms);
+browser.runtime.onConnect.addListener(tabComms);
