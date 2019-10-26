336b9de60bd3751ff9d8689d4c5044db257c7c70
MCManuelLP <mcmanuellp@gmail.com>
2019-10-26 11:58:28 +0200

    removed experimental drag n drop feature
    this already proved to be extremely fragile code, with only simple bookmarks, after the addition of folders I've decided to scrap this, to edit the order of everything you will have to use the browser's built-in bookmark manager or another extension

--- newtab.js
+++ newtab.js
@@ -1,5 +1,3 @@
-const DRAG_ENABLE = false;
-
 window.addEventListener("load", function () {
 	let templates = document.getElementById("bookmark-templates").content;
 	let bookmarkTemplate = templates.querySelector(".bookmark");
@@ -7,143 +5,6 @@ window.addEventListener("load", function () {
 	let bookmarkFolderTemplate = templates.querySelector(".bookmark-folder");
 	let bookmarkFolderItemTemplate = templates.querySelector(".bookmark-folder-item");
 	let bookmarkBar = document.querySelector(".bookmark-bar");
-
-	let getClosestBookmark = function(event) {
-		let target = event.target;
-
-		// TODO to do this properly, we totally should go through all,
-		//  since with different styling there's a chance that you can reach the bookmark bar
-		//  anywhere in the middle above or below the bookmarks, right now, to get around this
-		//  we strictly made the bookmark wrapper to get around this issue
-
-		if (target === bookmarkBar) {
-			// we are not over any existing bookmarks, so on either end of the row
-			// therefore we get the distance to the first bookmark
-			let first = bookmarkBar.firstElementChild;
-			let firstRect = first.getBoundingClientRect();
-			let firstDistX = Math.abs(event.pageX - firstRect.left);
-
-			// and also the distance to the last bookmark
-			let last = bookmarkBar.lastElementChild;
-			let lastRect = last.getBoundingClientRect();
-			let lastDistX = Math.abs(event.pageX - lastRect.right);
-
-			// and then we pick whichever is closest
-			if (firstDistX < lastDistX) {
-				target = first;
-			} else {
-				target = last;
-			}
-		} else {
-			// if we are over parts of a bookmark node, we want to get the parent bookmark
-			if (!target.closest) {
-				// this function is only available on node elements and not primitives like plain text
-				// these may well be the ones to respond to the event, therefore we need to move
-				// to the respective parent node, which in turn will provide the required function
-				target = target.parentNode;
-			}
-			// then we retrieve the closest parent node matching the selector (i.e. the surrounding bookmark div)
-			target = target.closest(".bookmark");
-		}
-
-		return target;
-	};
-
-	let positionDraggedBookmarkNode = function (event) {
-		let draggedNode = document.getElementById("dragged");
-		let target = getClosestBookmark(event);
-
-		// only do something if the dragged node isn't already in the right place
-		if (target !== draggedNode) {
-			// calculate relative offset on bookmark
-			let rect = target.getBoundingClientRect();
-			let offX = (event.pageX - rect.left) / rect.width;
-
-			if (offX > 0.5) {
-				// move to the next node and insert before it
-				target = target.nextElementSibling;
-				// check once more that we are not already placed correctly
-				if (target !== draggedNode) {
-					bookmarkBar.insertBefore(draggedNode, target);
-				}
-			} else if (target.previousElementSibling !== draggedNode) {
-				// final check if the dragged node isn't already in place, if not, move there
-				bookmarkBar.insertBefore(draggedNode, target);
-			}
-		}
-	};
-
-	if (DRAG_ENABLE) {
-		let handleBookmarkDragStart = function (event) {
-			event.dataTransfer.effectAllowed = "move";
-
-			// select target and mark it as the element to be dragged
-			let target = event.currentTarget;
-			target.id = "dragged";
-
-			// mark the location where the bookmark originates from, in case we want to cancel the drag operation
-			let originMarker = document.createElement("div");
-			originMarker.id = "origin-marker";
-			bookmarkBar.insertBefore(originMarker, target);
-
-			// I'm not sure that this is needed, but we set it anyway just in case
-			event.dataTransfer.setData("text/plain", target.innerText);
-		};
-
-		let handleBookmarkDragEnter = function (event) {
-			let draggedNode = document.getElementById("dragged");
-
-			if (bookmarkBar.contains(event.target)) {
-				// we are on the bookmark bar
-				// move the dragged bookmark
-				positionDraggedBookmarkNode(event);
-			} else {
-				// we are not on the bookmark bar
-				// return the dragged bookmark to its origin
-				// TODO we could check here if we aren't already there, for cases where the rest of the page
-				//  becomes more intricate such that this event fires more frequently but currently that is not necessary
-				let originMarker = document.getElementById("origin-marker");
-				bookmarkBar.insertBefore(draggedNode, originMarker);
-			}
-		};
-
-		let handleBookmarkDragOver = function (event) {
-			// prevent other mouse events such as selection while dragging
-			event.preventDefault();
-
-			// move the dragged bookmark
-			positionDraggedBookmarkNode(event);
-
-			// set the dropEffect to move
-			event.dataTransfer.dropEffect = "move";
-		};
-
-		let handleBookmarkDrop = function (event) {
-			// prevent other mouse events such as selection
-			event.preventDefault();
-
-			// this event fires when a bookmark has been dropped on the bookmark bar and is therefore already
-			// in the right position, we can simply remove the origin marker and let dragEnd handle the rest
-			let originMarker = document.getElementById("origin-marker");
-			bookmarkBar.removeChild(originMarker);
-		};
-
-		let handleBookmarkDragEnd = function (event) {
-			// node has been dropped, remove mark and styling
-			let draggedNode = document.getElementById("dragged");
-			draggedNode.removeAttribute("id");
-
-			let originMarker = document.getElementById("origin-marker");
-			if (originMarker) {
-				// if the bookmark hasn't been dropped on the designated target zone (bookmarkBar)
-				// the drop event doesn't fire, where the origin marker would be removed
-				// so if it still exists, we need to reposition the dragged bookmark first
-				bookmarkBar.insertBefore(draggedNode, originMarker);
-				// and then remove the origin marker
-				bookmarkBar.removeChild(originMarker);
-			}
-		};
-	}
 
 	let configureBookmark = function(bookmark, url, name, icon) {
 		// set base tooltip
@@ -187,28 +48,26 @@ window.addEventListener("load", function () {
 
 	let addBookmark = function (url, name, icon) {
 		let bookmark = document.importNode(bookmarkTemplate, true);
-		if (DRAG_ENABLE) {
-			bookmark.draggable = true;
-			bookmark.addEventListener("dragstart", handleBookmarkDragStart);
-			bookmark.addEventListener("dragend", handleBookmarkDragEnd);
-			// kinda doesn't go here, but whatever
-			bookmark.addEventListener("contextmenu", () => console.log("for later"));
-		}
 
 		configureBookmark(bookmark, url, name, icon);
 
 		bookmarkBar.appendChild(bookmark);
 	};
 
-	let hoverBookmarkItem = function (parentFolder, bookmarkItem) {
+	let hoverBookmarkItem = function (parentFolder) {
+		// get any open bookmark folder
 		let openFolderItem = parentFolder.querySelector(".bookmark-folder-item[open]");
+		// if one exists and has no close timeout yet
 		if (openFolderItem && !openFolderItem.closeTimeout) {
+			// add close timeout
 			openFolderItem.closeTimeout = window.setTimeout(function () {
 				toggleFolder(openFolderItem);
 				openFolderItem.closeTimeout = null;
 			}, 300);
 		}
+		// if the folder we're in has a close timeout
 		if (parentFolder.closeTimeout) {
+			// cancel that timeout
 			window.clearTimeout(parentFolder.closeTimeout);
 			parentFolder.closeTimeout = null;
 		}
@@ -218,7 +77,7 @@ window.addEventListener("load", function () {
 		let bookmarkItem = document.importNode(bookmarkItemTemplate, true);
 
 		let item = bookmarkItem.querySelector(".bookmark-layout");
-		item.addEventListener("mouseover", () => hoverBookmarkItem(folder, bookmarkItem));
+		item.addEventListener("mouseover", () => hoverBookmarkItem(folder));
 
 		configureBookmark(bookmarkItem, url, name, icon);
 
@@ -229,7 +88,6 @@ window.addEventListener("load", function () {
 	let toggleFolder = function(folder) {
 		let winLeft = 0;
 		let winRight = window.innerWidth;
-		let winTop = 0;
 		let winBottom = window.innerHeight;
 
 		let isOpen = folder.hasAttribute("open");
@@ -393,16 +251,8 @@ window.addEventListener("load", function () {
 
 	// ------------------------------------------------------------------------
 
-	if (DRAG_ENABLE) {
-		document.querySelector(".wrapper").addEventListener("dragenter", handleBookmarkDragEnter);
-
-		bookmarkBar.addEventListener("dragover", handleBookmarkDragOver);
-		bookmarkBar.addEventListener("drop", handleBookmarkDrop);
-	}
-
-	// ------------------------------------------------------------------------
-
-	// TODO we may want to handle the querying and handling of changeEvents on a site script
+	// TODO we may want to handle the querying and handling of bookmark changeEvents
+	//  on a site script
 
 	// TODO overflow menu, needs to change on resize, that is a bishh, would mean I need to
 	//  refactor the CSS and all, to only have one type of thing, abt overwrite lots with
